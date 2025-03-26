a75b54fb1f72f90a36c6735e884e3118d2f679f9
joaoferrariac <joaoferraridev@gmail.com>
2025-03-26 20:44:22 -0300

    feat: implementa settings.py com configurações da API

---
+++ models/settings.py
@@ -0,0 +1,8 @@
+class Settings:
+    API_KEY = "e6ad856db89e9a614fb028717b7c33b3"  # Substitua pela sua chave
+    BASE_URL = "http://api.openweathermap.org/data/2.5/"
+    UNITS = "metric"
+    LANG = "pt_br"
+    DEFAULT_CITY = "São Paulo"
+    
+    
